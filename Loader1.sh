b73574465b552714f5f33fa21b8dd9e5d72d83ae
WhatTheSemvak <54854607+WhatTheSemvak@users.noreply.github.com>
2019-10-13 06:45:48 +0700

    Update Loader1.sh

--- Loader1.sh
+++ Loader1.sh
@@ -7,7 +7,38 @@ echo -e "#SILAHKAN TUNGGU DAN LIHAT LIST TOOLS#" | lolcat
 echo -e "######################################" | lolcat
 
 sleep 2
+echo "\033[0;32m
+______________________
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+ ╔════════════╗
+       MR.Revoer'S
+ ╚════════════╝
+  ║██████████╚╗
+  ║██╔══╗█╔═╗█║
+  ║██║╬╔╝█╚╗║█║
+  ║██╚═╝█║█╚╝█║
+  ╚╗█████████═╝
+     ╚╗║╠╩╩╩╩╩╝
+       ║║┈┈┈█▐█████▒.｡oO
+       ║██╠╦╦╦╗
+       ╚╗██████";
 
+
+echo $yellow "+===========(MR.Revoer'S)===========+"
+echo $green "|+TOOLS  : ProgramV1              |"
+echo $green  "|+CODING : MR.Revoer'S         |"
+echo $yellow "|+CONTAC : 085736891082.       |"
+echo $yellow "|+THANKS TO : [•]C.A.I[•]       |"
+echo $yellow "|+Instagram : revoer_id   |"
+echo $yellow "|+Ngrecode Besok kencing nanah aminn |"
+echo $yellow "|~Special Thanks to MR.Revoer'S  |"
+echo $yellow "+==========[C.A.I]===========+"
+echo $blue ">>=========={MENU}==========<<"
 echo ""
 echo ""
 
@@ -20,13 +51,13 @@ sleep 1
 echo $g"==========================" | lolcat
 echo   "1.DDOS WEBSITE TERPROTEKSI" | lolcat
 echo $g"==========================" | lolcat
-sleep 0.8
+sleep 0.3
 echo ""
 echo $g"==========================" | lolcat
 echo   "2.Hack Fb Beragam Metode" | lolcat
 echo $g"==========================" | lolcat
 
-sleep 0.8
+sleep 0.3
 echo ""
 
 echo $g"==========================" | lolcat
@@ -34,20 +65,66 @@ echo   "3.Buat Script Deface Sendiri" | lolcat
 echo $g"==========================" | lolcat
 echo ""
 
-sleep 0.8
+sleep 0.3
 
 echo $g"==========================" | lolcat
 echo   "4.Install Tools MetaSploit" | lolcat
 echo $g"==========================" | lolcat
 
-sleep 0.8
+sleep 0.3
 echo ""
 
 echo $g"==========================" | lolcat
 echo   "5.Install NIK Reader Penipu" | lolcat
 echo $g"==========================" | lolcat
 
-sleep 0.7
+sleep 0.3
+echo ""
+echo $g"==========================" | lolcat
+echo   "6.Tools Dark Fb 😈 Setan" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.3
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "7.Google Translate In Termux" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.3
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "8.Kumpulan Terminals Terbaru" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.3
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "9.Hack Satelit Remote Control" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.3
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "10.Admin Login Url Finder" | lolcat
+echo $g"==========================" | lolcat
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "99.Install Bahan" | lolcat
+echo $g"==========================" | lolcat
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "00.Keluar/Minggat" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.3
+
+sleep 0.3
 
 echo $r"╭─[PILIH NOMORNYA]"
 read -p "╰─root@ivantamvans~# " pil
@@ -132,5 +209,115 @@ sh NIKPnp.sh
 
 fi
 
+if [ $pil = "6" ] || [ $pil = "06" ] 
+then
+
+figlet -f standard DarkFb | lolcat
+
+sleep 1
+
+git clone https://github.com/pashayogi/SETAN
+
+cd SETAN
+
+python2 SETAN.py
+
+fi
+
+if [ $pil = "7" ] || [ $pil = "07" ]
+then
+
+figlet -f standard  Translate | lolcat
+
+sleep 1
+
+git clone https://github.com/pashayogi/Translate
+
+cd Translate
+
+sh terj.sh
+
+fi
+
+if [ $pil = "8" ] || [ $pil = "08" ] 
+then
+
+figlet -f standard Terminals | lolcat
+
+sleep 1
+
+git clone https://github.com/pashayogi/TERMINALS/
+
+cd TERMINALS
+
+sh su.sh
+
+fi
 
 
+if [ $pil = "9" ] || [ $pil = "09" ] 
+then
+
+figlet -f standard Satelit | lolcat
+
+sleep 1
+
+echo "Ini Tools Abal Abal Ya Bambank :v"' 
+
+git clone https://github.com/pashayogi/satelit
+
+cd satelit
+
+sh hack-satelit.sh
+
+fi
+
+if [ $pil = 10 ]
+then
+
+figlet -f standard AdminFinder | lolcat
+
+sleep 1
+
+git clone https://github.com/the-c0d3r/admin-finder
+
+cd admin-finder
+
+python2 admin-finder.py
+
+fi
+
+if [ $pil = "99" ]
+then
+
+pkg install python2
+pkg install git
+pkg install nano
+pkg install php
+pkg install curl
+pkg install figlet
+pkg install ruby
+pkg install vim
+gem install lolcat
+pip2 install --upgrade pip
+pip2 install requests
+pip2 install mechanize
+pip2 install distribute
+
+echo "Semua Bahan Berhasil Di Install!!" | lolcat
+
+sh Loader1.sh
+
+fi
+
+if [ $pil = "00" ] || [ $pil = "0" ]
+then
+
+sleep 1
+
+echo "Tq Yang Sudah Pakai Tools Ini" | lolcat
+
+echo "Jangan Lupa Ser Ea" | lolcat
+exit
+
+fi
